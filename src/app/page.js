d1d983f6e018000793d3d8bbce828ed69ecb1bd4
RK Mañago <rbmanago1@up.edu.ph>
2024-04-27 14:28:29 +0800

    fix: change title, navbar, hero

--- src/app/page.js
+++ src/app/page.js
@@ -2,9 +2,12 @@
 
 export default function Home() {
   return (
-    <main className="min-h-[100svh] pt-[7rem] w-full">
+    <main className="min-h-[100svh] pt-[7rem] w-full border-x border-main">
       <div className="w-full">
-        HAHAASDMNASd
+        Hi, I am Arki. 
+      </div>
+      <div className="w-full">
+        I create designs that are on-target and on-brand.
       </div>
     </main>
   );
